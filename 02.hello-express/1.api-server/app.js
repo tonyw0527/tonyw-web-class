c8455f396ccc272d413c4f16fdb4400e42175fec
tonyw0527 <tonyw0527@gmail.com>
2020-12-02 14:40:25 +0900

    Updated 02

--- 02.hello-express/1.api-server/app.js
+++ 02.hello-express/1.api-server/app.js
@@ -36,6 +36,29 @@ app.get('/bible', (req, res) => {
     res.send(`From api server - requested params ${book} and ${chapter}`);
 })
 
+
+// use 메소드 - http 메소드 종류에 상관없이 첫번쨰 인자로 api가 들어가는 모든 경로에 대해 실행. next로 해당 경로와 일치는 메소드 실행
+// ex) /api/user, /api/say/hello 등등
+app.use('/api', (req, res , next) => {
+    console.log('hi');
+    next();
+})
+
+// 위에서 next();를 작성한 경우 아래 메소드 실행됨
+app.get('/api/hello', (req, res) => {
+    console.log('hello');
+})
+
+// all 메소드 - use처럼 http 메소드 종류에 상관없이 실행되지만 입력된 경로와 정확히 일치하는 경우만 실행된다.
+// ex) /api 에 대해서만 실행.
+app.all('/api', (req, res) => {
+
+});
+
+
+
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT} now!`);
 });
